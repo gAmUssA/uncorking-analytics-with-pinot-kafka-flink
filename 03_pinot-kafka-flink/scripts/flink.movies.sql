d3607ac861fa16d31a9088467032ca23d2c92993
Viktor Gamov <gamussa@gmail.com>
2024-05-06 22:21:38 -0700

    flink demo works

--- 03_pinot-kafka-flink/scripts/flink.movies.sql
+++ 03_pinot-kafka-flink/scripts/flink.movies.sql
@@ -24,30 +24,12 @@ SELECT *
 FROM Movies
 WHERE LOWER(title) LIKE '%lethal weapon%';
 
-
-CREATE TABLE MovieRatings
-(
-    movieId    INT,
-    rating DOUBLE,
-    ratingTime TIMESTAMP(3),                                      -- Using TIMESTAMP(3) to include millisecond precision
-    WATERMARK FOR ratingTime AS ratingTime - INTERVAL '5' SECONDS -- Adjust as needed based on your event-time characteristics
-) WITH (
-      'connector' = 'kafka',
-      'topic' = 'movie_ratings', -- Ensure this is the correct Kafka topic name for ratings
-      'properties.bootstrap.servers' = 'kafka:9092', -- Adjust for your Kafka setup
-      'scan.startup.mode' = 'latest-offset', -- Adjust if needed
-      'format' = 'json', -- Assuming JSON format for data
-      'json.timestamp-format.standard' = 'ISO-8601' -- Ensure this matches the format used in your data
-      );
-
-
 CREATE TABLE MovieRatings
 (
     movieId          INT,
     rating DOUBLE,
-    ratingTimeMillis BIGINT,                             -- Read the epoch milliseconds as BIGINT
-    ratingTime AS TO_TIMESTAMP_LTZ(ratingTimeMillis, 3), -- Convert to TIMESTAMP_LTZ
-    WATERMARK FOR ratingTime AS ratingTime - INTERVAL '5' SECONDS
+    ratingTimeMillis BIGINT,                            -- Read the epoch milliseconds as BIGINT
+    ratingTime AS TO_TIMESTAMP_LTZ(ratingTimeMillis, 3) -- Convert to TIMESTAMP_LTZ
 ) WITH (
       'connector' = 'kafka',
       'topic' = 'movie_ratings',
@@ -79,3 +61,15 @@ CREATE TABLE RatedMoviesSink
       'key.format' = 'json', -- Key format is JSON, matching the value format
       'value.format' = 'json' -- Values are serialized in JSON
       );
+
+INSERT INTO RatedMoviesSink
+SELECT m.movieId,
+       m.title,
+       m.releaseYear,
+       m.actors,
+       r.rating,
+       r.ratingTime
+FROM MovieRatings r
+         JOIN
+     Movies m
+     ON r.movieId = m.movieId;
